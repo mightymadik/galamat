dfeae7de17bfcb1a478c90e574015be13ca62647
Мадияр Бижан <madiarbizan@MacBook-Pro-Madiar.local>
2025-10-07 09:43:06 +0500

    Deploy Check

--- src/app/deployment-test/page.tsx
+++ src/app/deployment-test/page.tsx
@@ -30,7 +30,7 @@ export default function DeploymentTest() {
             </svg>
           </div>
           <p className="text-gray-600 mb-6">
-            Congratulations! Madiyar Your Next.js application is deployed and
+            Congratulations! Madiyar1 Your Next.js application is deployed and
             working correctly.
           </p>
         </div>
