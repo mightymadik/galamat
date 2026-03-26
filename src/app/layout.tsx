bbce1d6d8dcbbd38849afeb49cba457fccc4bffd
Мадияр Бижан <mightymadik@gmail.com>
2026-03-26 11:49:52 +0500

    Yandex metrika

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,7 +53,7 @@ export const viewport = {
 
 export default async function RootLayout({ children }: Props) {
   return (
-    <html>
+    <html lang="ru">
       <head>
         {/* Google Tag Manager */}
         <Script
@@ -70,17 +70,23 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 
         {/* Yandex.Metrika */}
         <Script
-          id="yandex-metrika-tag"
-          src="https://mc.yandex.ru/metrika/tag.js"
-          strategy="afterInteractive"
-        />
-        <Script
           id="yandex-metrika-init"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
-            __html: `window.ym=window.ym||function(){(window.ym.a=window.ym.a||[]).push(arguments)};
-window.ym.l=1*new Date();
-ym(108235655, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});`,
+            __html: `(function(m,e,t,r,i,k,a){
+  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+  m[i].l=1*new Date();
+  for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+})(window, document,'script','https://mc.yandex.ru/metrika/tag.js', 'ym');
+
+ym(108235655, 'init', {
+  webvisor:true,
+  clickmap:true,
+  trackLinks:true,
+  accurateTrackBounce:true,
+  ecommerce:"dataLayer"
+});`,
           }}
         />
       </head>
